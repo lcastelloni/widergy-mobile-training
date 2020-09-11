36dace6c41e81bf1d24d77231b0cfc882d85c4fb
Santiago José Dandois <santiago.dandois@widergy.com>
2020-09-11 16:09:01 -0300

    Linter options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:import/errors', 'plugin:import/warnings'],
+  plugins: ['import'],
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'no-console': 'error',
+    'no-unused-vars': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['external', ['builtin', 'internal'], 'parent', 'sibling', 'index'],
+      },
+    ],
+    'prettier/prettier': 'error',
+    'import/no-named-as-default': 0,
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+    'import/ignore': ['node_modules/react-native/index\\.js$'],
+  },
 };
